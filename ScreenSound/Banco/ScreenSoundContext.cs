8018521ab3928e836bfffb400c0bcbb617a29d6a
Victor Hugo Notari <victorhugonotari@gmail.com>
2024-04-11 16:24:52 -0300

    Criação do método MusicaDAL.

--- ScreenSound/Banco/ScreenSoundContext.cs
+++ ScreenSound/Banco/ScreenSoundContext.cs
@@ -6,6 +6,7 @@ namespace ScreenSound.Banco
     internal class ScreenSoundContext: DbContext
     {
         public DbSet<Artista>Artista { get; set; }
+        public DbSet<Musica> Musica { get; set; }
 
         private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSound;Integrated Security=True;Encrypt=True;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
 
