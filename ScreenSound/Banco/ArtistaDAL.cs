60642050cd18eead7139cc8dee25ea41b028f57b
Victor Hugo Notari <victorhugonotari@gmail.com>
2024-04-11 13:33:45 -0300

    Criação dos métodos Deletar e Atualizar

--- ScreenSound/Banco/ArtistaDAL.cs
+++ ScreenSound/Banco/ArtistaDAL.cs
@@ -11,7 +11,7 @@ namespace ScreenSound.Banco
             using var connection = new Connection().ObterConexao();
             connection.Open();
 
-            string sql = "SELECT * FROM Artista";
+            string sql = $"SELECT * FROM Artista";
             SqlCommand command = new SqlCommand(sql, connection);
             using SqlDataReader dataReader = command.ExecuteReader();
 
@@ -32,12 +32,42 @@ namespace ScreenSound.Banco
             using var connection = new Connection().ObterConexao();
             connection.Open();
 
-            string sql = "INSERT INTO Artista (Nome, FotoPerfil, Bio) VALUES (@nome, @perfilPadrao, @bio)";
+            string sql = $"INSERT INTO Artista (Nome, FotoPerfil, Bio) VALUES (@nome, @perfilPadrao, @bio)";
             SqlCommand command = new SqlCommand(sql, connection);
 
             command.Parameters.AddWithValue("@nome", artista.Nome);
             command.Parameters.AddWithValue("@perfilPadrao", artista.FotoPerfil);
             command.Parameters.AddWithValue("@bio", artista.Bio);
+
+            int retorno = command.ExecuteNonQuery();
+            Console.WriteLine($"Linhas afetadas: {retorno}");
+        }
+
+        public void Atualizar(Artista artista)
+        {
+            using var connection = new Connection().ObterConexao();
+            connection.Open();
+
+            string sql = $"UPDATE Artista SET Nome = @nome, Bio = @bio WHERE Id = @id";
+            SqlCommand command = new SqlCommand(sql, connection);
+
+            command.Parameters.AddWithValue("@nome", artista.Nome);
+            command.Parameters.AddWithValue("@perfilPadrao", artista.FotoPerfil);
+            command.Parameters.AddWithValue("@bio", artista.Bio);
+
+            int retorno = command.ExecuteNonQuery();
+            Console.WriteLine($"Linhas afetadas: {retorno}");
+        }
+
+        public void Deletar(Artista artista)
+        {
+            using var connection = new Connection().ObterConexao();
+            connection.Open();
+
+            string sql = $"DELETE FROM Artistas WHERE Id = @id";
+            SqlCommand command = new SqlCommand(sql, connection);
+
+            command.Parameters.AddWithValue("@id", artista.Id);
 
             int retorno = command.ExecuteNonQuery();
             Console.WriteLine($"Linhas afetadas: {retorno}");
