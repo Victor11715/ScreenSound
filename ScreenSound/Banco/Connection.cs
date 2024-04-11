fad23a49897e351aa8299ea6ea9f8c51275c08ca
Victor Hugo Notari <victorhugonotari@gmail.com>
2024-04-11 11:48:01 -0300

    Criação da consulta de Artista cadastrados no banco.

--- ScreenSound/Banco/Connection.cs
+++ ScreenSound/Banco/Connection.cs
@@ -1,9 +1,5 @@
 ﻿using Microsoft.Data.SqlClient;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using ScreenSound.Modelos;
 
 namespace ScreenSound.Banco
 {
@@ -14,6 +10,28 @@ namespace ScreenSound.Banco
         public SqlConnection ObterConexao()
         {
             return new SqlConnection(connectionString);
+        }
+
+        public IEnumerable<Artista> Listar()
+        {
+            var lista = new List<Artista>();
+            using var connection = ObterConexao();
+            connection.Open();
+
+            string sql = "SELECT * FROM Artista";
+            SqlCommand command = new SqlCommand(sql, connection);
+            using SqlDataReader dataReader = command.ExecuteReader();
+
+            while (dataReader.Read())
+            {
+                string nomeArtista = Convert.ToString(dataReader["Nome"]);
+                string bioArtista = Convert.ToString(dataReader["Bio"]);
+                int idArtista = Convert.ToInt32(dataReader["Id"]);
+                Artista artista = new(nomeArtista, bioArtista) { Id = idArtista };
+
+                lista.Add(artista);
+            }
+            return lista;
         }
     }
 }
